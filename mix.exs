83d7c1c9738a36921ce6f56cf7ae4ed374b95c58
Cam Cook <cam.cook.codes@gmail.com>
2025-06-22 22:10:53 -0400

    fix: GraphQL interface

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule Tunez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ash_authentication, "~> 4.0"},
       {:absinthe_phoenix, "~> 2.0"},
       {:ash_graphql, "~> 1.0"},
       {:open_api_spex, "~> 3.0"},
