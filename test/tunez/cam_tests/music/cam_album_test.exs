2545f0e2b734e53ad81c0f499000ca60c23534d5
Cam Cook <cam.cook.codes@gmail.com>
2025-06-26 23:19:43 -0400

    test: album test

--- test/tunez/cam_tests/music/cam_album_test.exs
+++ test/tunez/cam_tests/music/cam_album_test.exs
@@ -1,11 +1,10 @@
 defmodule TunezWeb.Music.CamAlbumTest do
   use Tunez.DataCase, async: true
   # use Oban.Testing, repo: Tunez.Repo
-  #  alias Ash.Error.Invalid
-  alias CamAlbum
+  alias Ash.Error.Invalid
   alias Tunez.Accounts.Notification, warn: false
   alias Tunez.Music, warn: false
-  #  alias Tunez.Music.Album
+  alias Tunez.Music.Album
 
   @moduletag :capture_log
 
@@ -13,12 +12,7 @@ defmodule TunezWeb.Music.CamAlbumTest do
     test desc(:create_album) do
       actor = build(:registered_user, insert?: true, role: :admin)
       artist = build(:artist, actor: actor, insert?: true)
-
-      album =
-        Music.create_album!(
-          %{name: "New Album", artist_id: artist.id, year_released: 2024},
-          actor: actor
-        )
+      album = build(:album, actor: actor, artist_id: artist.id, insert?: true)
 
       assert album.created_by_id == actor.id
       assert album.updated_by_id == actor.id
@@ -77,6 +71,57 @@ defmodule TunezWeb.Music.CamAlbumTest do
 
       assert Music.can_destroy_album?(editor, can_edit)
       refute Music.can_destroy_album?(editor, cant_edit)
+    end
+  end
+
+  describe desc(:validations) do
+    setup do
+      %{
+        artist: build(:artist, insert?: true),
+        admin: build(:registered_user, role: :admin, insert?: true)
+      }
+    end
+
+    test desc(:year_released), %{artist: artist, admin: admin} do
+      assert %{artist_id: artist.id, name: "test 2024", year_released: 2024}
+             |> Music.create_album!(actor: admin)
+
+      # # Using `assert_raise`
+      assert_raise Invalid, ~r/must be between 1950 and next year/, fn ->
+        %{artist_id: artist.id, name: "test 1925", year_released: 1925}
+        |> Music.create_album!(actor: admin)
+      end
+
+      # # Using `assert_has_error` - note the lack of bang to return the error
+      %{artist_id: artist.id, name: "test 1950", year_released: 1950}
+      |> Music.create_album(actor: admin)
+      |> Ash.Test.assert_has_error(Invalid, fn error ->
+        match?(%{message: "must be between 1950 and next year"}, error)
+      end)
+    end
+
+    test desc(:cover_image_url), %{artist: artist, admin: admin} do
+      with_url = fn url ->
+        Ash.Generator.action_input(Album, :create,
+          artist_id: artist.id,
+          year_released: 2025,
+          cover_image_url: url
+        )
+        |> Enum.at(0)
+      end
+
+      assert Music.create_album!(with_url.("/images/test.jpg"), actor: admin)
+
+      assert_raise Invalid, ~r/must start with/, fn ->
+        Music.create_album!(with_url.("notavalidURL"), actor: admin)
+      end
+
+      with_url.("/image/tunez.mp3")
+      |> Music.create_album(actor: admin)
+      |> assert_has_error(fn error ->
+        error.field == :cover_image_url &&
+          error.message == "must start with https:// or /images/"
+      end)
     end
   end
 end
